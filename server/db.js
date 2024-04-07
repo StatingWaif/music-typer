fb22284d5e8af14195a8a273280f3a53f5cd99bd
Виктор <StatingWaif68@gmail.com>
2024-04-07 22:26:13 +0300

    vercel

--- server/db.js
+++ server/db.js
@@ -5,7 +5,6 @@ module.exports = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    dialectModule: require("pg"),
     dialect: "postgres",
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
